d404231164d6e58daed9244f02a3fecaac81661b
Andi Tubagus <faatihkeane@gmail.com>
2025-03-09 17:16:47 +0800

    Update DailyTaskManagerSelection.java

--- DailyTaskManagerSelection.java
+++ DailyTaskManagerSelection.java
@@ -18,22 +18,32 @@ public class DailyTaskManagerSelection {
 
             switch (choice) {
                 case 1:
+                clearScreen();
                     System.out.println("\nRunning Array Mode...\n");
                     DailyTaskManagerArray.main(new String[]{}); // Directly calling main method
                     break;
                 case 2:
+                clearScreen();
                     System.out.println("\nRunning Linked List Mode...\n");
                     DailyTaskManagerLinkList.main(new String[]{}); // Directly calling main method
                     break;
                 case 3:
+                clearScreen();
                     run = false;
                     System.out.println("Exiting...");
                     break;
                 default:
+                clearScreen();
                     System.out.println("Invalid choice. Please try again.");
             }
         }
 
         scanner.close();
     }
+// Function for clearing the screen
+private static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    }
 }
+
